2fb99d29cd6387f55bbde6db6eebaa878bb1584a
JoJo10Smith <jordansmith912@gmail.com>
2023-09-23 12:35:50 -0700

    Added examples to the my_script.py and the notebook.ipynb for pandas Stata, changed the logic in the pandas_extensions.py StatWriter to use verion as int and not string when add convert_strl argument

--- examples/pandas/materialization/my_script.py
+++ examples/pandas/materialization/my_script.py
@@ -74,6 +74,12 @@ materializers = [
         buf="./df.html",
         combine=df_builder,
     ),
+    to.stata(
+        dependencies=output_columns,
+        id="df_to_stata",
+        path="./df.dta",
+        combine=df_builder,
+    ),
 ]
 # Visualize what is happening
 dr.visualize_materialization(
@@ -92,6 +98,7 @@ materialization_results, additional_outputs = dr.materialize(
         "df_to_sql_build_result",
         "df_to_xml_build_result",
         "df_to_html_build_result",
+        "df_to_stata_build_result",
     ],  # because combine is used, we can get that result here.
     inputs=initial_columns,
 )
@@ -101,5 +108,6 @@ print(additional_outputs["df_to_json_build_result"])
 print(additional_outputs["df_to_sql_build_result"])
 print(additional_outputs["df_to_xml_build_result"])
 print(additional_outputs["df_to_html_build_result"])
+print(additional_outputs["df_to_stata_build_result"])
 
 conn.close()
